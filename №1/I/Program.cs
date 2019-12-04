c2a1b4ed327dc43fd77851f8092ba351bfcd399a
Kirysha <57702306+KIRILLBRAGIN@users.noreply.github.com>
2019-12-04 20:26:03 +0300

    Add files via upload

---
+++ №1/I/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int p = 1;
+            int k = 0;
+            int b = 0;
+            while (a!=0)
+            { b=a%10;
+            k++;
+            sum+=b;
+            p*=b;
+            a = a/10;}
+            Console.WriteLine("Количество цифр равно " + k);
+            Console.WriteLine("Произведение цифр равно " + p);
+            Console.WriteLine("Сумма цифр равна " + sum);
+            Console.ReadKey(); }
+    }
+}
