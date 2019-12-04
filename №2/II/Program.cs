770232f125f59b0030bd2cd8e1f7bbf12189a285
Kirysha <57702306+KIRILLBRAGIN@users.noreply.github.com>
2019-12-04 20:27:28 +0300

    Add files via upload

---
+++ №2/II/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводите числа");
+            double sum = 0;
+            double p = 1;
+            double a=1;
+            while (a != 0)
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                if (a != 0)
+                {
+                    sum = sum + a;
+                    p = p * a;
+                }
+            }
+            Console.WriteLine("Сумма равна " + sum);
+            Console.WriteLine("Произведение равно " + p);
+            Console.ReadKey();
+        }
+    }
+}
