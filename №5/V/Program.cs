2364baf6e655f7e967e50efd27e6c4112f3e0cba
Kirysha <57702306+KIRILLBRAGIN@users.noreply.github.com>
2019-12-04 20:29:55 +0300

    Add files via upload

---
+++ №5/V/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string d = "Лось";
+            string a = "Волк";
+            string b = "волк";
+            string c = "ВОЛК";
+            while ((a != d) || (b != d) || (c != d))
+            {
+                Console.WriteLine("Не выступает в цирке.");
+                d = Console.ReadLine();
+                if ((a == d)||(b==d)||(c==d))
+                {
+                    Console.WriteLine("Правильно");
+                    Console.ReadKey();
+                }
+                else { Console.WriteLine("Неправильно, подумай получше"); }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ №6/VI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+            Console.WriteLine("Введите число n");
+            double n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Число разложенное на простые множители");
+            while (n > 0)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write(i + " ");
+                    n = n / i * 1.0;
+                }
+                else { i++; }
+            }
+            Console.ReadKey();
+        }
+    }
+}
